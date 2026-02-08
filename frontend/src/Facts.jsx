b34d25754d252c0cf8822427c72d49e3f2631def
Mahesh Kudleppanavar <maheshkudleppanavar@gmail.com>
2026-02-09 01:04:18 +0530

    UI fixed

--- frontend/src/Facts.jsx
+++ frontend/src/Facts.jsx
@@ -3,23 +3,45 @@ import './Facts.css';
 
 export default function Facts(){
 
-    const [facts,setFacts]=useState([]);
+    const [facts, setFacts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const getFacts = async () => {
-    try {
-        const response = await fetch('https://factz-backend.onrender.com/api/facts');
-        const data = await response.json();   
-        setFacts(data);
-    } 
-    catch (err) {
-        console.log(err);
-    }
-}
+        try {
+            setLoading(true);
+            setError(null);
+
+            const res = await fetch("https://factz-backend.onrender.com/api/facts");
+
+            if (!res.ok) {
+            throw new Error("Failed to fetch facts");
+            }
+
+            const data = await res.json();
+            setFacts(data);
+        } catch (err) {
+            setError("Oops! Unable to load facts. Please try again later.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+  
   return (
     <>
     <div className="container">
         <h1>Factz App</h1>
         <button onClick={getFacts}>Load Facts</button>
+
+        {loading && <p>Loading facts... ⏳</p>}
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        {!loading && !error && facts.length === 0 && (
+        <p> No facts available right now.</p>
+        )}
+
         <div className="facts-grid">
 
             {facts.map(fact => (
